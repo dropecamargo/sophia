cda1378d0f3323741896f0af6eafae593c67cda5
Pedro Camargo <dropecamargo@gmail.com>
2017-01-10 17:14:04 -0500

    Avance login

---
+++ resources/lang/es/app.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | App Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during all app KOI-TI 
+    |
+    */
+
+	'login' => 'Iniciar sesión',
+    'logout' => 'Cerrar sesión',
+
+    'exception' => 'Ha ocurrido un error inesperado por favor recarga tu navegador.',
+
+    'home' => 'Inicio',
+
+    'create' => 'Crear',
+    'save' => 'Guardar',
+    'edit' => 'Editar',
+    'comeback' => 'Regresar',
+    'cancel' => 'Cancelar',
+    'add' => 'Agregar',
+    'continue' => 'Continuar',
+
+    'pdf' => 'Generar PDF',
+    'xls' => 'Generar XLS',
+];
