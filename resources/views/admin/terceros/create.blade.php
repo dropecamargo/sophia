ddbea719d56e812d4eb3d716d1e79fec1d08efa0
Pedro Camargo <dropecamargo@gmail.com>
2017-02-15 22:56:54 -0500

    Bug editar producto - produccion

--- resources/views/admin/terceros/create.blade.php
+++ resources/views/admin/terceros/create.blade.php
@@ -1,6 +1,6 @@
 @extends('admin.terceros.main')
 
-@section('breadcrumb')	
+@section('breadcrumb')
 	<li><a href="{{ route('terceros.index') }}">Terceros</a></li>
 	<li class="active">Nuevo</li>
 @stop
@@ -8,14 +8,14 @@
 @section('module')
 	<div class="box box-danger" id="tercero-create">
 		{!! Form::open(['route' => 'terceros.store', 'id' => 'form-create-tercero', 'data-toggle' => 'validator', 'role' => 'form']) !!}
-			
+
 	        <div class="box-header with-border">
 	        	<div class="row">
 					<div class="col-md-2 col-sm-6 col-xs-6 text-left">
 						<a href="{{ route('terceros.index') }}" class="btn btn-default btn-sm btn-block">{{ trans('app.cancel') }}</a>
 					</div>
 					<div class="col-md-2 col-md-offset-8 col-sm-6 col-xs-6 text-right">
-						<button type="submit" class="btn btn-primary btn-sm btn-block">{{ trans('app.create') }}</button>
+						<button type="submit" class="btn btn-primary btn-sm btn-block">{{ trans('app.save') }}</button>
 					</div>
 				</div>
 			</div>
